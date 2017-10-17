48eaa8bfcb6669af00acdae021598792592ac27f
Harshit Singh Lodha <harshitandro@gmail.com>
2017-10-17 13:13:44 +0530

    Naive bayes mini project done
    Signed-off-by: Harshit Singh Lodha <harshitandro@gmail.com>

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB as nb
 
 
 ### features_train and features_test are the features for the training
@@ -21,13 +22,17 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-
-
+classifier = nb()
+t_start = time()
+classifier.fit(features_train,labels_train)
+print("Classifier training  took :{} sec".format(round(time()-t_start),4))
+t_start = time()
+score = classifier.score(features_test,labels_test)
+t_end = time()
+print("Accuracy : {}".format(score))
+print("Prediction took : {} sec".format(round(t_end-t_start,4)))
 #########################################################
 
 
